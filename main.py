740ee5f45c77f2011c336e87d0eab87833918374
vasanth <vasantkr97@gmail.com>
2025-10-24 12:00:49 +0530

    apis

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI 
 
+import json
+
 app = FastAPI()
+
+def load_data():
+    with open('patients.json', 'r') as f: #reading json file
+        data = json.load(f)
+    return data
 
 @app.get("/")
 def hello():
-    return {'message': "hellow world"}
+    return {'message': "patient management system api"}
 
 @app.get("/about")
 def about():
-    return {"message": "information python fastAPi"}
+    return {"message": "manage patients records"}
+
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
